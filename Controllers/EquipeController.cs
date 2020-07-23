636b2d870e9bdf1f82d544c93d811a148291eae7
ge-cyber-developer <61585687+ge-cyber-developer@users.noreply.github.com>
2020-07-23 11:34:35 -0300

    trabalho feito

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,11 +1,14 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Projeto_E_PLAYER.Models;
+
 
 namespace Projeto_E_PLAYER.Controllers
 {
@@ -16,6 +19,7 @@ namespace Projeto_E_PLAYER.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Equipes = equipeModel.ReadAll();
             return View();
         }
 
@@ -25,17 +29,47 @@ namespace Projeto_E_PLAYER.Controllers
             return View();
         }
 
-        public IActionResult Cadastrar(IformCollection form){
-        equipe.IdEquipe = Int32.Parse(form ["IdEquipe"] );
+        public IActionResult Cadastrar(IFormCollection form, Equipe equipe){
+            equipe.IdEquipe = Int32.Parse(form ["IdEquipe"] );
+
+            var file = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                equipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                equipe.Imagem   = "padrao.png";
+            }
+ 
         equipe.Nome = form["Nome"];
         equipe.Imagem  = form["Imagem"];
 
         equipeModel.Create(equipe);
 
-        ViewBag.Equipes = equipeModels.ReadAll();
-        return return LocalRedirect("~/equipe");
+        ViewBag.Equipes = equipeModel.ReadAll();
+        return  LocalRedirect("~/equipe");
+        }
+          
+        [Route("[controller]/{id}")]
+        public IActionResult Excluir (int id)
+        {
+          equipeModel.Delete(id);
+          return  LocalRedirect("~/equipe");
+
+
         }
     }
-
 
 }
